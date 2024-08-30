a9901c86296d5f24626aea3b4428e44f5a7c6723
Guy <guy@boardgamearena.com>
2024-08-30 15:07:19 +0200

    bga-zoom 1.0.4

--- gameinfos.inc.php
+++ gameinfos.inc.php
@@ -13,16 +13,7 @@
 $gameinfos = [ 
 
 // Name of the game in English (will serve as the basis for translation) 
-'game_name' => "Mind Up!",
-
-// Game designer (or game designers, separated by commas)
-'designer' => 'Maxime Rambourg',       
-
-// Game artist (or game artists, separated by commas)
-'artist' => 'Christine Alcouffe',         
-
-// Year of FIRST publication of this game. Can be negative.
-'year' => 2023,                 
+'game_name' => "Mind Up!",             
 
 // Game publisher (use empty string if there is no publisher)
 'publisher' => 'Catch Up Games',
@@ -86,19 +77,6 @@ $gameinfos = [
 // If an array of shortcode languages such as array( 1 => 'en', 2 => 'fr', 3 => 'it' ) then all players at the table must speak the same language, and this language must be one of the listed languages.
 // NB: the default will be the first language in this list spoken by the player, so you should list them by popularity/preference.
 'language_dependency' => false,
-
-// Complexity of the game, from 0 (extremely simple) to 5 (extremely complex)
-'complexity' => 2,    
-
-// Luck of the game, from 0 (absolutely no luck in this game) to 5 (totally luck driven)
-'luck' => 3,    
-
-// Strategy of the game, from 0 (no strategy can be setup) to 5 (totally based on strategy)
-'strategy' => 2,    
-
-// Diplomacy of the game, from 0 (no interaction in this game) to 5 (totally based on interaction and discussion between players)
-'diplomacy' => 3,    
-
 // Colors attributed to players
   'player_colors' => [
     'ff0000',
@@ -125,35 +103,13 @@ $gameinfos = [
 // Game interface width range (pixels)
 // Note: game interface = space on the left side, without the column on the right
 'game_interface_width' => [
-
+    'autoscale' => true,
+    
     // Minimum width
     //  default: 740
     //  maximum possible value: 740 (ie: your game interface should fit with a 740px width (correspond to a 1024px screen)
     //  minimum possible value: 320 (the lowest value you specify, the better the display is on mobile)
     'min' => 650,
-
-    // Maximum width
-    //  default: null (ie: no limit, the game interface is as big as the player's screen allows it).
-    //  maximum possible value: unlimited
-    //  minimum possible value: 740
-    'max' => null
   ],
 
-// Game presentation
-// Short game presentation text that will appear on the game description page, structured as an array of paragraphs.
-// Each paragraph must be wrapped with totranslate() for translation and should not contain html (plain text without formatting).
-// A good length for this text is between 100 and 150 words (about 6 to 9 lines on a standard display)
-'presentation' => [
-    totranslate("In Mind Up!, you start with a line of cards on the table, with as many cards as players. Each turn, players pick a card from their hand and simultaneously reveal it to make a new line, ordered from the lowest to the highest card. Each player then takes the card in the previous line at the same position as the one they just played, adding it to their tableau. These cards will score points at the end of the round, depending on their color and the order they were picked. After being scored, they become the player's hand for the next round."),
-],
-
-//////// BGA SANDBOX ONLY PARAMETERS (DO NOT MODIFY)
-
-// simple : A plays, B plays, C plays, A plays, B plays, ...
-// circuit : A plays and choose the next player C, C plays and choose the next player D, ...
-// complex : A+B+C plays and says that the next player is A+B
-'is_sandbox' => false,
-'turnControl' => 'simple'
-
-////////
 ];
